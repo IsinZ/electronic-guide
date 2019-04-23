45eaaea7bb78f1e171ea3feac3943180028e6dd7
megarungle <megarungle@gmail.com>
2019-04-24 00:26:19 +0300

    Added headers

--- src/infrastructure/IDatabaseInterfaces.py
+++ src/infrastructure/IDatabaseInterfaces.py
@@ -3,20 +3,25 @@ from abc import ABC, abstractmethod
 class IDatabaseBRM(ABC):
     # возвращает tuple(), содержащий:
     # 1. массив со всеми книгами (объекты book);
-    # 2. количество книг каждого типа (count).
+    # 2. количество книг каждого типа (count);
+    # 3. цена книг каждого типа (price);
+    # 4. скидка на книги каждого типа (discount).
     @abstractmethod
     def getBookCovers(self):
         """Get book covers"""
         
 class IDatabaseGUI(ABC):
-    # добавляет в базу данных информацию о новой книге.
+    # добавляет в базу данных информацию о новой книге,
+    # discount по умолчанию равен нулю.
     @abstractmethod
-    def addBook(self, book, count):
+    def addBook(self, book, count, price):
         """Add book"""
     
     # возвращает tuple(), содержащий:
     # 1. массив со всеми книгами (объекты book);
-    # 2. количество книг каждого типа (count).
+    # 2. количество книг каждого типа (count);
+    # 3. цена книг каждого типа (price);
+    # 4. скидка на книги каждого типа (discount).
     @abstractmethod
     def getAllBooks(self):
         """Get all books"""
