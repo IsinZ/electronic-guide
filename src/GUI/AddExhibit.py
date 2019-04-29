81999cba6d7a45da12af5c267e5db2cdfdf92e77
megarungle <43431514+megarungle@users.noreply.github.com>
2019-04-29 19:16:56 +0300

    Update AddExhibit.py

--- src/GUI/AddExhibit.py
+++ src/GUI/AddExhibit.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\KONSTANTIN\Documents\ui\AddExhibit.ui'
+# Form implementation generated from reading ui file 'AddExhibit.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
